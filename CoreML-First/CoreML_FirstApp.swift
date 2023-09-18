2d5e044b4a6d0aa1bd218fc29076026cb328903b
Fernando Putallaz <ferhedio@gmail.com>
2023-09-18 15:30:11 -0300

    Initial Commit

---
+++ CoreML-First/CoreML_FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_FirstApp.swift
+//  CoreML-First
+//
+//  Created by Fernando Putallaz on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
