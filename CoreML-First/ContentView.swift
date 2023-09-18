d6840461f8c94e5c7856502021ac580875ca3d86
Fernando Putallaz <ferhedio@gmail.com>
2023-09-18 20:21:32 -0300

    Image Picker implemented.

--- CoreML-First/ContentView.swift
+++ CoreML-First/ContentView.swift
@@ -6,16 +6,58 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var selectedImages: [PhotosPickerItem] = []
+    @State var data: Data?
+    @State var imageSelected = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    if let data, let uiImage = UIImage(data: data) {
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(30)
+                            .shadow(radius: 10)
+                            .padding()
+                    }
+                }
+                .navigationTitle(imageSelected ? "Your Image" : "Select an image")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        PhotosPicker(
+                            selection: $selectedImages,
+                            maxSelectionCount: 1,
+                            matching: .images
+                        ) {
+                            Image(systemName: "photo.badge.plus")
+                        }
+                    }
+                }
+                .onChange(of: selectedImages) { newValue in
+                    guard let item = selectedImages.first else {
+                        return
+                    }
+                    
+                    item.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let item):
+                            if let data = item {
+                                self.data = data
+                                imageSelected = true
+                            }
+                        case .failure(let error):
+                            fatalError("error \(error.localizedDescription)")
+                        }
+                    }
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
