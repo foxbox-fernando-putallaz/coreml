87e24782cbc48909213ba208640d11507709149b
Fernando Putallaz <fernando.putallaz@foxbox.com>
2023-09-27 12:17:06 -0300

    First (very bugg) live capture object detection

--- CoreML-First/CaptureView.swift
+++ CoreML-First/CaptureView.swift
@@ -7,18 +7,53 @@
 
 import AVFoundation
 import SwiftUI
+import Vision
 
 struct FrameView: View {
     var image: CGImage?
     private let label = Text("frame")
+    @State var isShowingAlert = false
+    @State var alertTitle = ""
     
     var body: some View {
         if let image {
             Image(image, scale: 1.0, orientation: .up, label: label)
+                .onChange(of: image) {
+                    detectImage(image)
+                }
         } else {
             Color.green
         }
-         
+    }
+    
+    func detectImage(_ cgImage: CGImage) {
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50(configuration: .init()).model) else {
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                isShowingAlert = true
+                alertTitle = "error loading the results"
+                return
+            }
+            let firstResult = results.first
+            
+            if let description = firstResult?.identifier, let confidence = firstResult?.confidence {
+                print("ml123 description: \(description)")
+                print("ml123 confidence: \(confidence)")
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        
+        do {
+            try handler.perform([request])
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
 
